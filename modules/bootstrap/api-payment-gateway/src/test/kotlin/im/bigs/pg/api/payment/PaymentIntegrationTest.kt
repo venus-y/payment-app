35ad93c1f6cc1dd5254ce6ad997061d4b5bdb8e3
venus01 <geumsung7769@naver.com>
2026-01-14 14:55:08 +0900

    feat: 결제 생성 통합 테스트 수정
    - 테스트의 시작 범위를 유스케이스에서 컨트롤러로 확장하여, 컨트롤러 - 유스케이스 - 영속성 계층의 실 흐름을 검증하였음

--- modules/bootstrap/api-payment-gateway/src/test/kotlin/im/bigs/pg/api/payment/PaymentIntegrationTest.kt
+++ modules/bootstrap/api-payment-gateway/src/test/kotlin/im/bigs/pg/api/payment/PaymentIntegrationTest.kt
@@ -1,22 +1,34 @@
 package im.bigs.pg.api.payment
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import im.bigs.pg.application.payment.port.`in`.PaymentCommand
-import im.bigs.pg.application.payment.port.`in`.PaymentUseCase
+import im.bigs.pg.application.payment.port.out.PaymentOutPort
+import im.bigs.pg.application.payment.port.out.PaymentQuery
 import im.bigs.pg.infra.persistence.payment.repository.PaymentJpaRepository
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.post
 import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
 import kotlin.test.assertEquals
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @Transactional
 class PaymentIntegrationTest {
 
     @Autowired
-    lateinit var paymentUseCase: PaymentUseCase
-    @Autowired lateinit var paymentJpaRepository: PaymentJpaRepository
+    lateinit var mockMvc: MockMvc
+
+    @Autowired
+    lateinit var objectMapper: ObjectMapper
+
+    @Autowired
+    lateinit var paymentOutPort : PaymentOutPort
 
     @Test
     fun `결제 생성 시 DB에 정책 기반 금액이 저장된다`() {
@@ -29,15 +41,21 @@ class PaymentIntegrationTest {
         )
 
         // when
-        val payment = paymentUseCase.pay(command)
+        mockMvc.post("/api/v1/payments") {
+            contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(command)
+        }.andExpect {
+            status { isOk() }
+        }
 
         // then
-        val saved = paymentJpaRepository.findById(payment.id!!)
-            .orElseThrow()
+        val page = paymentOutPort.findBy(PaymentQuery())
+        val saved = page.items.first()
 
         assertEquals(BigDecimal("10000"), saved.amount)
         assertEquals(BigDecimal("0.0235"), saved.appliedFeeRate)
         assertEquals(BigDecimal("235"), saved.feeAmount)
         assertEquals(BigDecimal("9765"), saved.netAmount)
     }
+
 }
